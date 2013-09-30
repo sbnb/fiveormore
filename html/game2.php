7427c5a33db41224dd46716ebcfd67c2c8a514b1
Sean Noble <sean.noble.72@gmail.com>
2013-09-30 09:37:20 +0200

    Add code to display adsense ads

--- html/game2.php
+++ html/game2.php
@@ -47,6 +47,22 @@
 
     <h1>Five or More</h1>
 
+    <center>
+      <div id="adsTop">
+        <script type="text/javascript"><!--
+        google_ad_client = "ca-pub-7635710845539038";
+        /* Top of game ad links */
+        google_ad_slot = "3552986507";
+        google_ad_width = 468;
+        google_ad_height = 15;
+        //-->
+        </script>
+        <script type="text/javascript"
+        src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+        </script>
+      </div>
+    </center>
+
     <table id="fiveormore">
     <thead>
     <tr>
